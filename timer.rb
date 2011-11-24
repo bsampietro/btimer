773cfbe0ffeb34e5853654f2768c19d803c0e1e0
Bruno Sampietro <bsampietro@gmail.com>
2011-11-23 22:11:47 -0200

    refactor and improvements

--- timer.rb
+++ timer.rb
@@ -11,25 +11,8 @@ def save
   end
 end
 
-#def duration
-#  secs  = self.to_int
-#  mins  = secs / 60
-#  hours = mins / 60
-#  days  = hours / 24
-
-#  if days > 0
-#    "#{days} days and #{hours % 24} hours"
-#  elsif hours > 0
-#    "#{hours} hours and #{mins % 60} minutes"
-#  elsif mins > 0
-#    "#{mins} minutes and #{secs % 60} seconds"
-#  elsif secs >= 0
-#    "#{secs} seconds"
-#  end
-#end
-
-def duration(elapsed_time)
-  secs  = elapsed_time.to_i
+def format(elapsed_seconds)
+  secs  = elapsed_seconds.to_i
   mins  = secs / 60
   hours = mins / 60
   fsecs = (secs % 60).to_s.rjust(2, '0')
@@ -45,39 +28,42 @@ def duration(elapsed_time)
   end
 end
 
+# get number of seconds from a time str like 'xx:xx:xx'
 def get_seconds(str)
   time = str.split(':')
   time[0].to_i * 3600 + time[1].to_i * 60 + time[2].to_i
 end
 
-def display_elapsed
-  seconds_passed = @data[:state] == :started ? (TIME_NOW - @data[:start]) + @data[:elapsed] : @data[:elapsed]
-  puts duration(seconds_passed)
+def elapsed_seconds
+  @data[:state] == :started ? TIME_NOW - @data[:start] + @data[:elapsed] : @data[:elapsed]
 end
 
 @data = YAML.load_file(FILE_PATH) rescue {:start => TIME_NOW, :elapsed => 0, :state => :stopped}
 
-if ARGV.first == "start"
-  if @data[:state] == :stopped
+case ARGV.first
+  when "start"
+    if @data[:state] == :stopped
+      @data[:start] = TIME_NOW
+      @data[:state] = :started
+      save
+    end
+  when "stop"
+    if @data[:state] == :started
+      @data[:state] = :stopped
+      @data[:elapsed] += (TIME_NOW - @data[:start]).to_i
+      save
+    end
+  when "reset"
+    @data[:elapsed] = 0
     @data[:start] = TIME_NOW
     @data[:state] = :started
     save
-  end
-elsif ARGV.first == "stop"
-  if @data[:state] == :started
-    @data[:state] = :stopped
-    @data[:elapsed] += TIME_NOW - @data[:start]
+  when "set"
+    @data[:elapsed] = get_seconds ARGV[1]
+    @data[:start] = TIME_NOW
     save
-  end
-elsif ARGV.first == "reset"
-  @data[:elapsed] = 0
-  @data[:start] = TIME_NOW
-  save
-elsif ARGV.first == "set"
-  @data[:elapsed] = get_seconds ARGV[1]
-  @data[:start] = TIME_NOW
-  save
 end
 
+
 puts @data[:state].to_s
-display_elapsed
+puts format(elapsed_seconds)
